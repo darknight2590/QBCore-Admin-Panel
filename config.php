98d4ba737eb122b54519675796bfd7e6906145ce
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-16 22:53:56 +0100

    Created Start of Config File

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('SERVER_NAME', 'QBCore Panel'); // Your Server Name (Better short for the tab) (This will also show on a Discord embed)
+define('SERVER_DESCRIPTION', 'Description about your server!'); // Description of your server (This will also show on a Discord embed)
+
+?>
