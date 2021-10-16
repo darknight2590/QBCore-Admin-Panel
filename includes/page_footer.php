d382e52f5a02a03516c112cd8f49c915be1128ca
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-16 22:54:06 +0100

    Created includes

---
+++ includes/page_footer.php
@@ -0,0 +1,15 @@
+<div class="app-wrapper-footer">
+<div class="app-footer">
+    <div class="app-footer__inner">
+        <div class="app-footer-right">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="https://github.com/aidanohart" target="_blank" class="nav-link">
+                        QBCore Admin Panel made by aidanohart
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</div>
---
+++ includes/page_navigationBar.php
@@ -0,0 +1,19 @@
+<div class="scrollbar-sidebar">
+<div class="app-sidebar__inner">
+    <ul class="vertical-nav-menu">
+        <li class="app-sidebar__heading">Dashboards</li>
+            <li>
+                <a href="index.php" class="mm-active">
+                    <i class="metismenu-icon pe-7s-note"></i>
+                    Dashboard
+                </a>
+
+                <a href="login.php">
+                    <i class="metismenu-icon pe-7s-rocket"></i>
+                    Dashboard Example 2
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+</div>    
