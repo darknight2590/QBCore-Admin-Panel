1a20ea3519d226d51454a6689b8a48979f44b4f4
aidanohart <44532573+aidanohartt@users.noreply.github.com>
2021-10-16 22:54:15 +0100

    Created main page

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+require_once(__DIR__ . "/config.php");
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- DO NOT TOUCH THESE -->    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <link href="./main.css" rel="stylesheet">
+
+    <!-- YOU CAN EDIT THESE IN CONFIG.PHP, NOT HERE -->    
+    <title><?php echo SERVER_NAME;?> | Dashboard</title>
+    <meta name="description" content="<?php echo SERVER_DESCRIPTION;?>">
+    <meta name="author" content="https://github.com/aidanohart">
+</head>
+
+<body>
+
+<!-- DONT TOUCH ANYTHING BELOW HERE -->
+<div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
+    <div class="app-header header-shadow">
+        <div class="app-header__logo">
+            <div class="logo-src"></div>
+            <div class="header__pane ml-auto">
+                <div>
+                    <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                        <span class="hamburger-box">
+                            <span class="hamburger-inner"></span>
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="app-header__mobile-menu">
+            <div>
+                <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+        </div>
+        <div class="app-header__menu">
+            <span>
+                <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                    <span class="btn-icon-wrapper">
+                        <i class="fa fa-ellipsis-v fa-w-6"></i>
+                    </span>
+                </button>
+            </span>
+        </div>    
+        </div>        
+            <div class="app-main">
+            <div class="app-sidebar sidebar-shadow">
+                <div class="app-header__logo">
+                    <div class="logo-src"></div>
+                    <div class="header__pane ml-auto">
+                        <div>
+                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                                <span class="hamburger-box">
+                                    <span class="hamburger-inner"></span>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="app-header__mobile-menu">
+                    <div>
+                        <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                            <span class="hamburger-box">
+                                <span class="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                    
+                <!-- NAVIGATION BAR -->
+                <?php include "includes/page_navigationBar.php"; ?> 
+
+                <!-- DASHBOARD PAGE -->
+                <?php include "pages/dashboard.php"; ?> 
+
+                <!-- FOOTER HERE -->
+                <?php include "includes/page_footer.php"; ?> 
+
+                </div>
+            </div>
+        </div>
+    <script type="text/javascript" src="./assets/scripts/main.js"></script>
+    </body>
+</html>
---
+++ pages/dashboard.php
@@ -0,0 +1,63 @@
+<div class="app-main__outer">
+<div class="app-main__inner">
+<div class="app-page-title">
+<div class="page-title-wrapper">
+    <div class="page-title-heading">
+        <div class="page-title-icon">
+            <i class="pe-7s-car icon-gradient bg-qb-core">
+            </i>
+        </div>
+        <div>Admin Panel Dashboard
+            <div class="page-title-subheading">This dashboard shows all statistics about your server and is the main home page of the admin panel!
+            </div>
+        </div>
+    </div>  
+</div>
+</div>            
+<div class="row">
+<div class="col-md-6 col-xl-4">
+    <div class="card mb-3 widget-content">
+        <div class="widget-content-outer">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Total Characters</div>
+                    <div class="widget-subheading">Total number of unique characters created</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-danger">a number</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-6 col-xl-4">
+    <div class="card mb-3 widget-content">
+        <div class="widget-content-outer">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Total Bans</div>
+                    <div class="widget-subheading">Total number of unique bans served</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-danger">a number</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-6 col-xl-4">
+    <div class="card mb-3 widget-content">
+        <div class="widget-content-outer">
+            <div class="widget-content-wrapper">
+                <div class="widget-content-left">
+                    <div class="widget-heading">Total Notes</div>
+                    <div class="widget-subheading">Total number of unique notes served</div>
+                </div>
+                <div class="widget-content-right">
+                    <div class="widget-numbers text-danger">a number</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
